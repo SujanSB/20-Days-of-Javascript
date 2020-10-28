64e03a59c4176b289560d807676aaaae9669a038
InvalidSB <invalidsb45@gmail.com>
2020-10-28 23:07:35 +0545

    Day one finished

---
+++ Day-1/main.js
@@ -0,0 +1,149 @@
+console.log("Variabe");
+// var (comment)
+// var name ="praman"
+// var thar ='Ghimire'
+// console.log(name+ thar) 
+//javascript always recognize any variable as a string at first
+
+// var x = "10"
+// console.log(typeof(x)) 
+
+// console.log(x)
+// var x = 10
+
+// var y =5
+// var z= x+y 
+// console.log(typeof(x)) 
+
+// console.log(x)
+
+
+// console.log("mileko xa")
+
+
+// criteria for declaring a variable
+// #number bata start hudaina 
+// #case sensative
+// Can start with _
+
+
+// camalCase
+// snake_case
+
+// console.log("const")
+
+// const x =10
+// console.log(x)
+// x =6 
+//paidaina
+
+
+
+
+console.log("Conditional Renderng")
+
+// if else 
+
+// let age = 25
+// = means equel to,value assign garxa
+// == means comparing
+
+// if(age == 25){
+//     console.log("budo vais bihe gar");
+// }
+// else{
+//     console.log("bla bla bla")
+// }
+
+// Switch
+
+// switch(age){
+//     case 20:
+//         console.log("milena");
+//         break;
+//     case 25:
+//         console.log("milyo");
+//         break;
+//     default:
+//         console.log("ta xadde vaai nagar")
+// }
+
+
+console.log("function")
+
+// String literals
+// `${variable_name}`
+
+// const age =48
+// console.log(`My age is  ${age}`)
+
+
+// var name = "Praman"
+// console.log("this is outside of function")
+// function Myfunction(name,age){
+//         if(age == 45){
+//             console.log(name) 
+//             console.log(age)
+
+//         }
+//         else{
+//             console.log("milena")
+//         }
+// }
+// Myfunction(name,45)
+// console.log("aba arko")
+// Myfunction(name ,67)
+// Myfunction()
+
+
+console.log("Object")
+// Object is everything in javascript
+
+// var kura ={
+//     name:"Sujan",
+//     thar :"Sharma",
+//     age :function(year){
+    //      var age = 2020-year
+//         console.log("your age is " )
+//         console.log(age)
+//     }
+// }
+// console.log(kura)
+// console.log(kura.name)
+
+// kura.age(1999)
+
+console.log("hoisting")
+
+// variable must be declare above of function call "Concept of hoisting"
+
+// var z = 10
+// Yeuta() //yo line le paila kaam garxa
+// function Yeuta(){
+//     console.log(z)
+//     console.log("yeuta function vitra")
+// }
+
+console.log("Scoping")
+
+// is variable accessible ?
+
+let x = 10
+function Yeuta(){
+
+}
+
+console.log("______________________________________________________________________________________")
+
+function Arko(){
+    Yeuta()
+    console.log(x)
+}
+Arko()
+
+
+console.log("That's it for TODAY")
+
+
+
+
