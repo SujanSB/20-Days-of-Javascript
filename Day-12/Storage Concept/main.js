f1f54f7480bfaccc82290321718898d0eefe28ed
InvalidSB <invalidsb45@gmail.com>
2020-11-08 21:57:22 +0545

    localstorage and session storage complete

---
+++ Day-12/Storage Concept/main.js
@@ -0,0 +1,45 @@
+// console.log("LOcal storage and session storage")
+
+// localStorage.setItem("sujan","name")
+
+// let name = localStorage.getItem('sujan')
+
+// console.log( typeof name)
+// // let suj = localStorage.getItem('')
+// localStorage.removeItem('name')
+// let  xyz= localStorage.getItem('xyz')
+// // console.log(xyz)
+
+
+// localStorage.setItem("subject","javascript")
+
+
+
+// // array lai kasari localstorage ma rakhne ?
+
+// let array = ['red','blue','green','pink']
+
+
+// localStorage.setItem('color',array)
+
+// let arrayConsole = localStorage.getItem('color')
+// console.log(arrayConsole)
+// // console.log(typeof  arrayConsole)
+
+
+
+// localStorage.setItem('color',JSON.stringify(array))
+
+// let consoleArray = localStorage.getItem('color')
+// console.log( typeof consoleArray)
+
+// let cars = JSON.parse(localStorage.getItem('color'))
+// console.log( typeof cars)
+
+
+// localStorage.clear()
+
+// // session storage vaneko chai k vanda current time ma brower open vainjjel ko session ma matra session storage ma store garera rakhne
+
+
+
