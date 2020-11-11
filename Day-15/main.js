3903731bbe7bb8ff2177b629792deb1fed9d664c
InvalidSB <invalidsb45@gmail.com>
2020-11-11 22:19:52 +0545

    day 15 finished

--- Day-15/main.js
+++ Day-15/main.js
@@ -1,3 +1,9 @@
+
+// promise
+// -reject 
+// -resolve
+
+
 // const probj1 = new Promise((resolve,reject)=>{
 
 //     setTimeout(()=>{
@@ -5,12 +11,17 @@
 //         resolve(roll_no)
 //         // reject('error aayo yrr brother')
 //     },2000)
-
 // })
 
-// probj1.then((nom)=>{
+
+// Thapa Technical
+
+// probj1
+// .then((nom)=>{
 //     console.log(nom)
-// }).catch((err)=>{
+    
+// })
+// .catch((err)=>{
 //     console.log(err)
 // })
 
@@ -22,27 +33,38 @@
 // getData()
 
 
+// Json -- Net ninja/code with with harry
 
 
 
-fetch('https://api.covid19api.com/summary')
-.then(responseData=>{
-    return responseData.json();
-})
-.then((data)=>{
-    // console.log(data)
-    // console.log(data.Countries)
-    console.log(data.Countries[120])
-    // const Data = data.Countries[120]
-    // document.getElementById("yaha").innerHTML=`${Data.Country} has ${Data.NewConfirmed} confirmed cases Today.`
-})
-
+// fetch('https://api.covid19api.com/summary')
+// .then(responseData=>{
+//     return responseData.json();
+// })
+// .then((data)=>{
+//     // console.log(data)
+//     // console.log(data.Countries)
+//     console.log(data.Countries[120])
+//     const Data = data.Countries[120]
+//     document.getElementById("yaha").innerHTML=`${Data.Country} has ${Data.NewConfirmed} confirmed cases Today.`
+// })
+// .catch((err)=>{
+//     console.log(err)
+// })
 
 
 // getData = async()=>{
 //    const responseData= await fetch('https://api.covid19api.com/summary')
 //    const data = await responseData.json();
+//     const Data = data.Countries[120]
+//     document.getElementById("yaha").innerHTML=`${Data.Country} has ${Data.NewConfirmed} confirmed cases Today.Total Case ${Data.TotalConfirmed}`
+
 
 //    console.log(data)
 // } 
 // getData()
+
+
+
+
+// Day 15 finished
